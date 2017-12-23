25f500007816ac1ce44e98a5de4bc41439530dec
theeam <eduuardomuller@gmail.com>
2017-12-23 14:03:29 -0300

    Section 2, Lecture 18

--- app/src/main/java/com/example/theeam/appdemo/MainActivity.java
+++ app/src/main/java/com/example/theeam/appdemo/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.theeam.appdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void loginClicked(android.view.View){
+    public void loginClicked(View view){
         EditText username = findViewById(R.id.username); //recebe o username
         EditText password = (EditText) findViewById(R.id.password); //recebe o password
 
         Log.i("username", username.getText().toString()); //printa no log o username
         Log.i("password", password.getText().toString()); //printa no log o password
+
+        Toast.makeText(this, "Welcome, " + username.getText().toString(), Toast.LENGTH_LONG).show();
+
     }
 
     @Override
