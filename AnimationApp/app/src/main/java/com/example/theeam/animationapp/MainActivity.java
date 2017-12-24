a4eb74ba629585326cdef40feee19456c1cd4cfb
theeam <eduuardomuller@gmail.com>
2017-12-24 13:41:15 -0300

    Section 4, Lecture 47

--- AnimationApp/app/src/main/java/com/example/theeam/animationapp/MainActivity.java
+++ AnimationApp/app/src/main/java/com/example/theeam/animationapp/MainActivity.java
@@ -20,9 +20,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void translation(View view){
+        ImageView lula = findViewById(R.id.lulaImg);
+        ImageView bolso = findViewById(R.id.bolsoImg);
+
+        bolso.animate().alpha(1f).setDuration(1000);
+        lula.animate().translationXBy(1000f).setDuration(1500);
+        lula.animate().translationYBy(2000f).setDuration(2500);
+        bolso.animate().translationXBy(-1000f).setDuration(5000);
+    }
+
+    public void rotation(View view){
+        ImageView lula = findViewById(R.id.lulaImg);
+        ImageView bolso = findViewById(R.id.bolsoImg);
+
+        lula.animate().rotation(180f).setDuration(2500);
+    }
+
+    public void scale(View view){
+        ImageView lula = findViewById(R.id.lulaImg);
+        ImageView bolso = findViewById(R.id.bolsoImg);
+
+        lula.animate().scaleX(0.5f).scaleY(0.5f).setDuration(1500);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView lula = findViewById(R.id.lulaImg);
+        lula.setTranslationX(-1000f);
     }
 }
